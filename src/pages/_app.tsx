51af07c1bf873edac94cb3274c84a030931fa083
Felipe Salomao <lipesalomaoclaf@gmail.com>
2022-06-12 19:56:05 -0300

    created config

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { globalStyles } from "../../styles/global";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {globalStyles()}
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
